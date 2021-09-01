bcc0a2e7777238720d9bad8c0a2ab16a9d988c8e
Kaung Myat Han <arkarhtethan@gmail.com>
2021-09-02 01:20:40 +0630

    logged out route added

---
+++ src/routers/logged-out-router.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CreateAccount from "../pages/create-account";
+import Login from "../pages/login";
+
+const LoggedOutRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/create-account">
+          <CreateAccount />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default LoggedOutRouter;

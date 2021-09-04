94214bce716ae4c05d4299120fc1db39e4495d29
Kaung Myat Han <arkarhtethan@gmail.com>
2021-09-05 00:47:41 +0630

    design changed and my profile link updated to /edit-profile from /my-profile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,15 +14,17 @@ const Header: React.FC = () => {
   return (
     <>
       {!data?.me.verified && (
-        <div className="bg-red-500 p-3 text-center text-white text-sm">
+        <div className="bg-red-500 p-3 text-center text-white text-base">
           <span>Please verify your email.</span>
         </div>
       )}
       <header className="500 py-4">
         <div className="w-full px-5 xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
-          <img src={nuberLogo} className="w-24 " alt="Nuber Eats" />
+          <Link to="/">
+            <img src={nuberLogo} className="w-24 " alt="Nuber Eats" />
+          </Link>
           <span className="text-xs">
-            <Link to="/my-profile/">
+            <Link to="/edit-profile/">
               <FontAwesomeIcon icon={faUser} className="text-xl" />
             </Link>
           </span>

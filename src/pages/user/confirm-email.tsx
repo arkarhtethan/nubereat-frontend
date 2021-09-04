34256ed4f09c0aa1e8dec371ed6995db48b8d21d
Kaung Myat Han <arkarhtethan@gmail.com>
2021-09-05 00:48:04 +0630

    Confirm email page added

---
+++ src/pages/user/confirm-email.tsx
@@ -0,0 +1,68 @@
+import { gql, useApolloClient, useMutation } from "@apollo/client";
+import React, { useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import { useMe } from "../../hooks/useMe";
+import {
+  verifyEmail,
+  verifyEmailVariables,
+} from "../../__generated__/verifyEmail";
+
+const VERIFY_EMAIL_MUTATION = gql`
+  mutation verifyEmail($input: VerifyEmailInput!) {
+    verifyEmail(input: $input) {
+      ok
+      error
+    }
+  }
+`;
+
+const ConfirmEmail: React.FC = () => {
+  const client = useApolloClient();
+  const { data: userData } = useMe();
+  const history = useHistory();
+  const onCompleted = (data: verifyEmail) => {
+    const {
+      verifyEmail: { ok },
+    } = data;
+    if (ok && userData?.me.id) {
+      client.writeFragment({
+        id: `User:${userData.me.id}`,
+        fragment: gql`
+          fragment VerifiedUser on User {
+            verified
+          }
+        `,
+        data: { verified: true },
+      });
+      history.push("/");
+    }
+  };
+  const [verifyEmail] = useMutation<verifyEmail, verifyEmailVariables>(
+    VERIFY_EMAIL_MUTATION,
+    {
+      onCompleted,
+    }
+  );
+  const location = useLocation();
+  useEffect(() => {
+    const [_, code] = location.search.split("=");
+    verifyEmail({
+      variables: {
+        input: {
+          code,
+        },
+      },
+    });
+  }, []);
+
+  return (
+    <div className="mt-52 flex flex-col items-center justify-center">
+      <h2 className="text-lg mb-2 font-medium">Confirming email...</h2>
+      <h4 className="text-gray-700 text-sm">
+        Please wait, don't close this page...
+      </h4>
+    </div>
+  );
+};
+
+export default ConfirmEmail;

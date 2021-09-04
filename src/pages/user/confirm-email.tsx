4140519cc3074d3ddf7792e1d3e971f328103166
Kaung Myat Han <arkarhtethan@gmail.com>
2021-09-05 00:57:58 +0630

    helmet added

--- src/pages/user/confirm-email.tsx
+++ src/pages/user/confirm-email.tsx
@@ -1,5 +1,6 @@
 import { gql, useApolloClient, useMutation } from "@apollo/client";
 import React, { useEffect } from "react";
+import { Helmet } from "react-helmet-async";
 import { useHistory, useLocation } from "react-router-dom";
 import { useMe } from "../../hooks/useMe";
 import {
@@ -57,6 +58,9 @@ const ConfirmEmail: React.FC = () => {
 
   return (
     <div className="mt-52 flex flex-col items-center justify-center">
+      <Helmet>
+        <title>Confirm Email | Nuber Eats</title>
+      </Helmet>
       <h2 className="text-lg mb-2 font-medium">Confirming email...</h2>
       <h4 className="text-gray-700 text-sm">
         Please wait, don't close this page...

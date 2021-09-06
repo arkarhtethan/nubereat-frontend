c86206205bc8f266c39dcda3c5b4b6819d487c70
Kaung Myat Han <arkarhtethan@gmail.com>
2021-09-06 19:31:54 +0630

    Link added to restaurant component

--- src/components/restaurant.tsx
+++ src/components/restaurant.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 interface IRestaurantProps {
   id: number;
   coverImage: string;
@@ -13,16 +14,18 @@ const Restaurant: React.FC<IRestaurantProps> = ({
   categoryName,
 }) => {
   return (
-    <div key={id} className="flex flex-col">
-      <div
-        className="py-28 bg-cover bg-center mb-3"
-        style={{ backgroundImage: `url(${coverImage})` }}
-      ></div>
-      <h3 className="text-xl font-medium">{name}</h3>
-      <span className="border-t mt-2 text-xs opacity-50 py-2 border-gray-300">
-        {categoryName}
-      </span>
-    </div>
+    <Link to={`/restaurant/${id}`}>
+      <div key={id} className="flex flex-col">
+        <div
+          className="py-28 bg-cover bg-center mb-3"
+          style={{ backgroundImage: `url(${coverImage})` }}
+        ></div>
+        <h3 className="text-xl font-medium">{name}</h3>
+        <span className="border-t mt-2 text-xs opacity-50 py-2 border-gray-300">
+          {categoryName}
+        </span>
+      </div>
+    </Link>
   );
 };
 

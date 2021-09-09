6087cf784267c568915f3a4e0d0310ca770d5bba
Kaung Myat Han <arkarhtethan@gmail.com>
2021-09-10 00:30:47 +0630

    code refactored

--- src/pages/__test__/404.spec.tsx
+++ src/pages/__test__/404.spec.tsx
@@ -1,18 +1,10 @@
-import { render, waitFor } from "@testing-library/react";
 import React from "react";
-import { HelmetProvider } from "react-helmet-async";
-import { BrowserRouter } from "react-router-dom";
+import { waitFor, render } from "../../test-utils";
 import PageNotFound from "../404";
 
 describe("<PageNotFound/>", () => {
   it("renders OK", async () => {
-    render(
-      <HelmetProvider>
-        <BrowserRouter>
-          <PageNotFound />
-        </BrowserRouter>
-      </HelmetProvider>
-    );
+    render(<PageNotFound />);
     await waitFor(() => {
       expect(document.title).toBe("Page Not Found | Nuber Eats");
     });

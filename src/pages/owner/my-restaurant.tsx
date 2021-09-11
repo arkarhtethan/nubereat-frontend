5035cadab546d4d46ef4fc53d4d88cbe75a8f4e5
Kaung Myat Han <arkarhtethan@gmail.com>
2021-09-11 20:02:28 +0630

    data & ui added to my-restaurant.tsx

--- src/pages/owner/my-restaurant.tsx
+++ src/pages/owner/my-restaurant.tsx
@@ -1,23 +1,28 @@
 import { gql, useQuery } from "@apollo/client";
 import React from "react";
-import { useParams } from "react-router-dom";
-import { RESTAURANT_FRAGMENT } from "../../fragment";
+import { Link, useParams } from "react-router-dom";
+import Dish from "../../components/dish";
+import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragment";
 import {
   myRestaurant,
   myRestaurantVariables,
 } from "../../__generated__/myRestaurant";
 
-const MY_RESTAURANT_QUERY = gql`
+export const MY_RESTAURANT_QUERY = gql`
   query myRestaurant($input: MyRestaurantInput!) {
     myRestaurant(input: $input) {
       ok
       error
       restaurant {
         ...RestaurantParts
+        menu {
+          ...DishParts
+        }
       }
     }
   }
   ${RESTAURANT_FRAGMENT}
+  ${DISH_FRAGMENT}
 `;
 
 interface IParams {
@@ -34,8 +39,41 @@ const MyRestaurant = () => {
       },
     }
   );
-  console.log(data);
-  return <div>MyRestaurant</div>;
+  return (
+    <div>
+      <div
+        className="bg-gray-700 py-28 bg-center bg-cover"
+        style={{
+          backgroundImage: `url(${data?.myRestaurant.restaurant?.coverImage})`,
+        }}
+      ></div>
+      <div className="container mt-10 px-5">
+        <div className="text-2xl font-medium mb-10">
+          {data?.myRestaurant.restaurant?.name}
+        </div>
+        <Link
+          to={`/restaurant/${id}/add-dish`}
+          className="mr-8 text-white bg-gray-800 py-3 px-10"
+        >
+          Add Dish &rarr;
+        </Link>
+        <Link to={``} className="mr-8 text-white bg-lime-700 py-3 px-10">
+          Buy Promotion &rarr;
+        </Link>
+        <div className="mt-5">
+          {data?.myRestaurant.restaurant?.menu?.length === 0 ? (
+            <h4 className="text-xl mb-5">Please upload a dish.</h4>
+          ) : (
+            <div className="grid md:grid-cols-3 gap-x-5 gap-y-10 mt-10 m-5">
+              {data?.myRestaurant.restaurant?.menu?.map((dish) => (
+                <Dish {...dish} />
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyRestaurant;
